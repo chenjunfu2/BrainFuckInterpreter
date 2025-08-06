23f774f5b5e18f88e5ea5627f35a54ef62298c0b
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-08-07 06:06:39 +0800

    报错信息完善：增加行号与列号

--- BrainFuckInterpreter/Preprocessor.hpp
+++ BrainFuckInterpreter/Preprocessor.hpp
@@ -47,6 +47,10 @@ public:
 		//块语句栈（存储索引）
 		std::vector<size_t> codeBlockStack;
 
+		//报错信息
+		size_t szLine = 1;
+		size_t szColumn = 1;
+
 		//读取并转换到代码列表
 		while (true)
 		{
@@ -56,6 +60,9 @@ public:
 			{
 				break;//读完离开
 			}
+
+			//每读一个字符增加一列
+			++szColumn;
 
 			CodeUnit CurCode{};
 			switch (cRead)
@@ -113,7 +120,7 @@ public:
 				{
 					if (codeBlockStack.empty())//什么，根本没有左括号，哪来的右括号，去你的
 					{
-						printf("解析失败：括号未匹配\n");
+						printf("解析失败[line:%zu,column:%zu]：括号未匹配\n", szLine, szColumn);
 						return false;
 					}
 
@@ -132,7 +139,7 @@ public:
 					//在此之前，先判断一下szStackTop够不够szDupCount，不够说明括号未配对，报错
 					if (szDupCount > szStackTop)
 					{
-						printf("解析失败：括号未匹配\n");
+						printf("解析失败[line:%zu,column:%zu]：括号未匹配\n", szLine, szColumn);
 						return false;
 					}
 
@@ -198,16 +205,46 @@ public:
 				}
 				continue;//注意此处为continue而非break，不走下面默认压入
 			default:
-				if (isspace(cRead))
+				//处理行号
 				{
-					continue;//跳过空白，直接继续
-				}
-				//其余未知字符且不在注释内，报错
-				
-				printf("解析失败：遇到未知字符:[%c]\n", cRead);
+					bool bNewLine = false;
 
-				//todo报错：行号字符数
-				return false;//注意报错直接返回
+					if (cRead == '\r')//判断下一个是不是\n
+					{
+						bNewLine = true;
+						sFile.Read(cRead);
+						if (!sFile.Eof() && cRead != '\n')
+						{
+							sFile.MovFilePos(-1);//回退1字节
+						}//此处哪怕eof也不用担心，更新后就会continue，然后退出循环
+					}
+					else if (cRead == '\n')
+					{
+						bNewLine = true;
+					}
+
+
+					if (bNewLine)
+					{
+						++szLine;
+						szColumn = 1;
+						continue;//直接继续
+					}
+
+					//不是换行，处理其余空白
+					if (isspace(cRead))
+					{
+						continue;//跳过空白，直接继续
+					}
+					
+
+					//都不是，那么未知字符且不在注释内，报错
+					printf("解析失败[line:%zu,column:%zu]：遇到未知字符:[%c]\n", szLine, szColumn, cRead);
+
+					//todo报错：行号字符数
+					return false;//注意报错直接返回
+				}
+				break;
 			}
 
 			//压入列表
@@ -217,7 +254,7 @@ public:
 		//判断栈内是否还有残留的括号，是，则代表括号未配对，报错
 		if (!codeBlockStack.empty())
 		{
-			printf("解析失败：括号未匹配\n");
+			printf("解析失败[line:%zu,column:%zu]：括号未匹配\n", szLine, szColumn);
 			return false;
 		}
 
