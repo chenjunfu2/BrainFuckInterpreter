d0b958ddb9e0fa02110c16aa2cf48b3a19c8f2b2
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-08-07 05:34:28 +0800

    修改为命令行参数

--- BrainFuckInterpreter/main.cpp
+++ BrainFuckInterpreter/main.cpp
@@ -3,14 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-	//if (argc != 2)
-	//{
-	//	printf("仅接受一个brainfuck文件名参数！");
-	//	return 0;
-	//}
+	if (argc != 2)
+	{
+		printf("仅接受一个brainfuck文件名参数！");
+		return 0;
+	}
 
-	//Interpreter itp(argv[1]);
-	Interpreter itp("test.bf");
+	Interpreter itp(argv[1]);
 	itp.run();
 
 	return 0;
